15ffd5f51e927ac7677becafeea20a6817084dbb
Virgínia Arruda <vica_arruda@hotmail.com>
2021-03-25 21:40:57 -0300

    Adiciona exercicio 9

---
+++ bloco_4/dia_2/exercicio9.js
@@ -0,0 +1,17 @@
+let array = [];
+
+for (let index = 1; index <= 25; index += 1) {    
+    array.push(index);
+}
+
+// array = [1 ... 25]
+for (let index = 0; index < array.length; index += 1) {    
+   
+   console.log(array[index] / 2);
+}
+
+
+
+
+
+ 
