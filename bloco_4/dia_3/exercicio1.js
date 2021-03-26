827eefa85176c494c5156764b8fbf0d568c76db7
Virgínia Arruda <vica_arruda@hotmail.com>
2021-03-26 11:56:04 -0300

    Adiciona exercicio 1

---
+++ bloco_4/dia_3/exercicio1.js
@@ -0,0 +1,11 @@
+let n = 6;
+let asterisco = '*';
+let line = '';
+
+for(let index = 0; index < n; index += 1){
+    line = line + asterisco;
+}
+
+for (let index = 0; index < n; index += 1){
+    console.log(line)
+}
