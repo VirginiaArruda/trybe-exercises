b94bfdc3972be9dd6d52b6c137480961ede777e9
Virgínia Arruda <vica_arruda@hotmail.com>
2021-04-01 16:36:03 -0300

    Adiciona exercicio 5 - Parte II

--- bloco_4/dia_4/ParteII/exercicio5.js
+++ bloco_4/dia_4/ParteII/exercicio5.js
@@ -1,11 +1,26 @@
-function returnNumber(array) {
-  let repetition = 0;
-  for (let index = 0; index < array.length; index += 1) {
-    if (array[index] === repetition)  {
-      repetition += 1 }
-  } 
-  return repetition;   
+function maisRepetido(numeros) {
+  let num = {};
 
+  for (let index = 0; index < numeros.length; index += 1) {
+    let valor = numeros[index];
+    if (num[valor] === undefined) {
+      num[valor] = 1;
+    } else {
+      num[valor] = num[valor] + 1;
+    }
+  }
+
+  let contRepetido = 0;
+  let contNumero = 0;
+
+  for (let key in num) {
+    if (contRepetido < num[key]) {
+      contRepetido = num[key];
+      contNumero = key;
+    }
+  }
+
+  return contNumero;
 }
 
-console.log(returnNumber([2, 3, 2, 5, 8, 2, 3]))
+console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3]));
