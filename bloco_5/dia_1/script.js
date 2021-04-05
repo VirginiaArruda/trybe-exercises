ed016cc35304a753cd346725e027e3132deaae13
Virgínia Arruda <vica_arruda@hotmail.com>
2021-04-05 15:13:33 -0300

    Adiciona cor azul

--- bloco_5/dia_1/script.js
+++ bloco_5/dia_1/script.js
@@ -4,3 +4,8 @@ headerContainer.style.background = 'green';
 let emergencyTasks = document.getElementsByClassName("emergency-tasks");
 emergencyTasks[0].style.background = 'brown';
 
+let emergencyTasks2 = document.querySelectorAll(".emergency-tasks h3");
+for (let index = 0; index < emergencyTasks2.length; index += 1){
+  emergencyTasks2[index].style.background = 'blue';
+}
+
