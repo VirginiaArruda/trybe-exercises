3a1f18dff8c4bc4c6a16e145ae86c978fc51db49
Virgínia Arruda <vica_arruda@hotmail.com>
2021-04-05 14:47:54 -0300

    Adiciona cor marrom

--- bloco_5/dia_1/script.js
+++ bloco_5/dia_1/script.js
@@ -1,1 +1,6 @@
-console.log(document.getElementById("header-container").style.background = 'green');
+let headerContainer = document.getElementById("header-container");
+headerContainer.style.background = 'green';
+
+let emergencyTasks = document.getElementsByClassName("emergency-tasks");
+emergencyTasks[0].style.background = 'brown';
+
